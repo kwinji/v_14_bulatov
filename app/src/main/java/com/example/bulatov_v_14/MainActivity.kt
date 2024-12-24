68ef1acd151b21cb31826d4b19a92e849f44a532
kwinji <gengarkkk@mail.ru>
2024-12-24 17:31:15 +0300

    last

--- app/src/main/java/com/example/bulatov_v_14/MainActivity.kt
+++ app/src/main/java/com/example/bulatov_v_14/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
@@ -242,8 +241,3 @@ fun DeleteMovieScreen(movieList: List<String>, onMovieDeleted: (String) -> Unit,
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MovieBookingApp()
-}
